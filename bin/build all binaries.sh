576710f60761a75b340830224fb98b039b138200
Ori Hoch <ori@uumpa.com>
2019-06-24 10:18:05 +0300

    build zip / tar.gz archives containing short file name suitable for the OS

--- bin/build all binaries.sh
+++ bin/build all binaries.sh
@@ -19,7 +19,21 @@ build() {
     docker build -t "${DOCKER_IMAGE}" -f ./Dockerfile.build .
   fi &&\
   docker push "${DOCKER_IMAGE}" &&\
-  docker run --rm -v "`pwd`:/go/src/github.com/cloudwm/cli" "${DOCKER_IMAGE}" go build -o cloudcli-${GOOS}-${GOARCH}${EXT} main.go
+  docker run --rm -v "`pwd`:/go/src/github.com/cloudwm/cli" "${DOCKER_IMAGE}" \
+    go build -o cloudcli${EXT} main.go
+  [ "$?" != "0" ] && return 1
+  if [ "${EXT}" == ".exe" ]; then
+    zip cloudcli-${GOOS}-${GOARCH}.zip cloudcli${EXT} &&\
+    echo Great Success! &&\
+    echo Created cloudcli binary archive: cloudcli-${GOOS}-${GOARCH}.zip
+  else
+    [ "${EXT}" != "" ] && return 1
+    tar -czvf cloudcli-${GOOS}-${GOARCH}.tar.gz cloudcli &&\
+    echo && echo '******************************' &&\
+    echo Great Success! &&\
+    echo Created cloudcli binary archive: cloudcli-${GOOS}-${GOARCH}.tar.gz &&\
+    echo '******************************' && echo
+  fi
 }
 
 build darwin 386 "" &&\
@@ -28,4 +42,4 @@ build linux 386 "" &&\
 build linux amd64 "" &&\
 build windows 386 ".exe" &&\
 build windows amd64 ".exe" &&\
-echo Great Success!
+echo && echo && echo Great Success! All binaries compiled and archived
